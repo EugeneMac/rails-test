e6d03cc28df56a731ae4bf9456d1665c1228cc14
Eugene M <ujeen1980@gmail.com>
2013-09-26 18:29:21 +0300

    Gemfile update for strong parameters

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,17 +1,17 @@
 class PostsController < ApplicationController
   def new
-    
+    @post = Post.new
   end
   
   def create
-  @post = Post.new(post_params)
+    @post = Post.new(params[:post].permit(:title, :text))
  
-  @post.save
-  redirect_to @post
+    @post.save
+    redirect_to @post
   end
   
   def show
-  @post = Post.find(params[:id])
+    @post = Post.find(params[:id])
   end
     
   private

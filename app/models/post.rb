3a7f180b0670c9461a1b400bc1bd82dc22653acd
Eugene M <ujeen1980@gmail.com>
2013-09-26 20:14:08 +0300

    Tags added

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,8 @@
 class Post < ActiveRecord::Base
   has_many :comments, dependent: :destroy
-  attr_accessible :text, :title
+  has_many :tags
   validates :title, presence:true, length: {minimum:5}
+  attr_accessible :text, :title, :tags_attributes
+  accepts_nested_attributes_for :tags, :allow_destroy => :true,
+    :reject_if => proc { |attrs| attrs.all? { |k, v| v.blank? } }
 end
